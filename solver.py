5d46dd5a62643cd4f89567e730ee389f6c55c310
happytorry <torleiv.njaa@gmail.com>
2026-03-27 17:02:33 +0100

    påske!

--- solver.py
+++ solver.py
@@ -2,18 +2,38 @@ from collections import Counter
 
 with open("solutions.txt", "r") as f:
     words = f.read().splitlines()
-letters = "".join(words)
-mycount = Counter(letters).most_common(6)  
-print (mycount)
-top_letters = [letter for letter, count in mycount]
-print (top_letters)
 
-for n in words:
+
+position_counts = [Counter() for _ in range(5)]
+
+for word in words:
+    for i, letter in enumerate(word):
+        position_counts[i][letter] += 1
+
+#print(position_counts)
+
+#for i in range(5):
+    #print(i, position_counts[i].most_common(5))
+
+
+
+for word in words:
+    word_score = 0
+    for i, letter in enumerate(word):
+        word_score += position_counts[i][letter]
+    print (word, word_score)
+
+
+
+#print(word_scores)
+#print(i, word_scores[i].most_common(5))
+
+
+
+def score_word(word):
     score = 0
-    seenletters= []    
-    for m in n:
-        if m in top_letters and m not in seenletters:
-            score +=1
-            seenletters.append(m)
-        if score == 5:
-            print (n, score)
+    for i, letter in enumerate(word):
+        score += position_counts[i][letter]
+    return score
+
+#print(score_word("crane"))
